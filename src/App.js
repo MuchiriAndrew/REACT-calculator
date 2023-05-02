f532a922e6d17e2e566ddcd32451aca0c7d09f87
MuchiriAndrew <kariukia225@gmail.com>
2023-05-02 18:06:15 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -153,13 +153,7 @@ function formatOperand(operand) {
 
 function App() {
 
-  
-
-const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer,
-  {}
-  )
-
-
+const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
 
   return (
     <div className="calculator-grid">
